0fe4bb96c9ebb0541f20c2fac6ae908b61980fcb
boojo <jshim27@wisc.edu>
2022-07-13 21:17:43 -0500

    cleaned up output formatting

--- fake_insta.py
+++ fake_insta.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess
 import getpass
+import os
 
 import instaloader
 from alive_progress import alive_bar
@@ -10,11 +11,6 @@ from termcolor import cprint
 from pyfiglet import figlet_format
 
 imports = ["instaloader", "alive-progress", "pyfiglet", "termcolor", "colorama"]
-
-for im in imports:
-    if im in sys.modules is False:
-         subprocess.check_call([sys.executable, '-m', 'pip', 'install', im])
-
 
 class InstaUser:
     def __init__(self, username, password):        
@@ -27,12 +23,14 @@ class InstaUser:
         self.followers = []
         self.following = []
         
-        with alive_bar(self.profile.get_followers().count, title="\nLooking through your followers") as bar:
+        with alive_bar(self.profile.get_followers().count, title="Looking through your followers") as bar:
             for followers in self.profile.get_followers():
                 self.followers.append(followers.username)
                 bar()
+                
+        print("\n")
         
-        with alive_bar(self.profile.get_followees().count, title="\nJudging who you follow") as bar: 
+        with alive_bar(self.profile.get_followees().count, title="Judging who you follow") as bar: 
             for followee in self.profile.get_followees():
                 self.following.append(followee.username)
                 bar()
@@ -48,22 +46,30 @@ class InstaUser:
         fake_friends = list(set(self.following)-set(self.followers))
         
         if (len(fake_friends) == 0):
-            print("Everyone follows you back!")
+            print("\n\n🎉 Everyone follows you back! 🎉")
             return
-        
-        return fake_friends
+        else:
+            print("\n\n🚨 These are your fake 'friends' 🚨\n")
+            for fake in fake_friends:            
+                print('💀 {}\n'.format(fake))
+            return
     
-def main():    
+def main():
+    for im in imports:
+        if im in sys.modules is False:
+                subprocess.check_call([sys.executable, '-m', 'pip', 'install', im])
+    
     subprocess.run("clear")
     cprint(figlet_format('fakestagram', font='speed'), 'blue')
     cprint("Created by Joonbo Shim\n", 'blue')
     
     USER = input("Enter your Instagram Username: ")
     PASSWORD = getpass.getpass("\nEnter your Instagram Password: ")
+    print('\n')
 
     user = InstaUser(USER, PASSWORD)
                     
-    print(user.getFake())
+    user.getFake()
 
 if __name__ == "__main__":
     main()
